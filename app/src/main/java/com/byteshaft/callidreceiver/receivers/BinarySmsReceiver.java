e820094e36d10f188b1bef1d14f2d8773535148f
scorpion1 <shgr8rb@gmail.com>
2015-10-19 18:15:21 +0500

    create text file WIP

--- app/src/main/java/com/byteshaft/callidreceiver/receivers/BinarySmsReceiver.java
+++ app/src/main/java/com/byteshaft/callidreceiver/receivers/BinarySmsReceiver.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 
 import com.byteshaft.callidreceiver.utils.Helpers;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
 
 public class BinarySmsReceiver extends BroadcastReceiver {
@@ -13,12 +16,20 @@ public class BinarySmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String message = Helpers.decodeIncomingSmsText(intent);
-        String[] content = message.split(",");
-        String number = content[0];
-        String name = Helpers.getContactName(context, number);
-        String dateTime = DateFormat.getInstance().format(Long.valueOf(content[1]));
-        String data = String.format("Contact ID: %s\nCall time: %s", name, dateTime);
-        Helpers.writeMessageContentToFile(number+ "-" +
-                Helpers.getTimeStamp(Long.valueOf(content[1])), data);
+        try {
+            JSONObject object = new JSONObject(message);
+            String state = (String) object.get("state");
+            if (state.equals("new")) {
+                String name = Helpers.getContactName(context, (String) object.get("number"));
+                String dateTime = DateFormat.getInstance().format(object.get("time"));
+                String data = String.format("Contact ID: %s\nCall time: %s", name, dateTime);
+                Helpers.writeMessageContentToFile((String) object.get("time"), data);
+            } else if (state.equals("old")) {
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
